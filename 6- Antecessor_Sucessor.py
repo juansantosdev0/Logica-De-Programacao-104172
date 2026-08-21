8b613fe70f3603b21998410f1e269d8ccd028069
juansantosdev0 <juan.figueiredo@ba.estudante.senai.br>
2026-08-21 16:41:35 -0300

    Add files via upload

---
+++ 6- Antecessor_Sucessor.py
@@ -0,0 +1,21 @@
+import os
+
+#limpa terminal
+os.system('cls')
+
+print(' = solicitando dados = ')
+numero = int(input("digite um numero: "))
+
+antecessor = numero - 1 
+sucessor = numero + 1
+
+
+
+
+
+
+print('\n= Exibindo Dados =')
+
+print("numero: ", numero)
+print("antecessor: ", antecessor)
+print("sucessor: ", sucessor)
