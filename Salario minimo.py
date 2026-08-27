05d146b69b61e46b0dda56e1559a2e00ee16a501
Juan.Santos <juan.figueiredo@ba.estudante.senai.br>
2026-08-27 14:16:57 -0300

    Add files via upload

---
+++ Salario minimo.py
@@ -0,0 +1,8 @@
+import os
+os.system ("cls")
+
+salario = float(input("Digite o valor do salário: "))
+minimo = float ("1621.00")
+
+salario = salario / minimo
+print("O valor do salário é: ", salario)
