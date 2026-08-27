a3bba6998c0dc56c2da401f6146664534f4122e3
Juan.Santos <juan.figueiredo@ba.estudante.senai.br>
2026-08-27 13:46:11 -0300

    Add files via upload

---
+++ 1- calculadora.py
@@ -0,0 +1,22 @@
+import os
+os.system ("cls")
+numero1 = int(input("digite um numero: "))
+numero2 = int(input("digite um numero: "))
+
+resultado = float
+
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+
+print("O primeiro número é: ", numero1)
+print("O segundo número é: ", numero2)
+
+
+
+print("A soma é: ", soma)
+print("A subtração é: ", subtracao)
+print("A multiplicação é: ", multiplicacao)
+print("A divisão é: ", divisao)
