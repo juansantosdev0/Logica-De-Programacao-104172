e16d2e75fafb6e6380bab3c1bd2de40e7fabc542
Juan.Santos <juan.figueiredo@ba.estudante.senai.br>
2026-09-04 16:40:44 -0300

    Add files via upload

---
+++ intervalo de 10 a 20 .py
@@ -0,0 +1,13 @@
+import os
+os.system("cls")
+
+
+numero = int(input("escolha um numemero: "))
+
+
+
+if numero >= 10 and numero <= 20:
+    print(f"{numero} dentro do intervalo entre 10 a 20 : ")
+
+else:
+    print("fora do intervalo: ")
