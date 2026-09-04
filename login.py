51a24189c2a502ab1fbd2dadb260901cd12d330c
Juan.Santos <juan.figueiredo@ba.estudante.senai.br>
2026-09-04 15:15:37 -0300

    Add files via upload

---
+++ login.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls")
+
+usuario =str(input("digite seu login:"))
+senha = float(input("dgite sua senha"))
+
+if usuario == "Juan":
+    print("Logado com sucesso sej bem-vindo senhor: ")
+elif senha == 102008:
+    print("Bem-vindo:")
+else:
+    print("logi ou senha invalido:")
